1b7115e0a7b787e8ea8c08e00bc49c153c6f7d9a
Alexandra Seelig <108905816+lolxela@users.noreply.github.com>
2024-11-07 20:18:37 -0500

    Added data classes that store form related information

--- composeApp/src/commonMain/kotlin/com/dev/database/Entity/Entity.kt
+++ composeApp/src/commonMain/kotlin/com/dev/database/Entity/Entity.kt
@@ -1,8 +1,50 @@
+package com.dev.database.Entity
+
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toInstant
 import kotlinx.datetime.toLocalDateTime
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+
+
+// type of fields that the user can add to their form
+@Serializable
+enum class FieldType {
+    SHORT_STRING,
+    LONG_STRING,
+    DROPDOWN,
+    MULTI_SELECT
+    //IMAGE_UPLOAD     // For uploading images of sample, to implement later.
+}
+
+// Each field is stored in the field table.
+// These are the fields that users can add when creating a new form.
+@Serializable
+data class Field(
+    @SerialName("field_name") val fieldName: String,
+    @SerialName("field_type") val fieldType: FieldType,
+    @SerialName("is_required") val isRequired: Boolean = false,
+    @SerialName("dropdown_options") val dropdownOptions: List<String>? = null,
+    @SerialName("multi_select_options") val multiSelectOptions: List<String>? = null
+)
+
+// Stores the forms that the user has created (NOT form entries, just  forms themselves).
+@Serializable
+data class SampleForm(
+    @SerialName("form_name") val formName: String,
+    @SerialName("date_created_utc") val dateCreatedUTC: String,
+    @SerialName("location") val location: String,
+    @SerialName("fields") val fields: List<Field>
+)
+
+// Stores actual form entries
+@Serializable
+data class SampleData(
+    @SerialName("form_name") val formName: String,
+    @SerialName("date_collected_utc") val dateCollectedUTC: String,
+    @SerialName("location") val location: String,
+    @SerialName("data_entries") val dataEntries: Map<String, String> //First value contains field name, second value contains user input
+)
 
 @Serializable
 data class RocketLaunch(
