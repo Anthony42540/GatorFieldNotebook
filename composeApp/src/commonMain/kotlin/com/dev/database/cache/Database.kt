6a68a379608bece3a122bf1ccd7334878d896354
Alexandra Seelig <108905816+lolxela@users.noreply.github.com>
2024-11-29 02:38:57 -0500

    Added getter functions into db class (retrieves data from db to display)

--- composeApp/src/commonMain/kotlin/com/dev/database/cache/Database.kt
+++ composeApp/src/commonMain/kotlin/com/dev/database/cache/Database.kt
@@ -1,6 +1,134 @@
 package com.dev.database.cache
+import com.dev.database.entity.Field
+import com.dev.database.entity.FieldType
+import com.dev.database.entity.SampleForm
+import com.dev.database.entity.SampleData
+import com.dev.database.entity.DataEntry
+import com.dev.database.entity.SampleAndData
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 internal class Database(databaseDriverFactory: DatabaseDriverFactory) {
     private val database = AppDatabase(databaseDriverFactory.createDriver())
     private val dbQuery = database.appDatabaseQueries
+
+    //get a list of all sample forms (to choose from when adding new sample)
+    internal fun getAllSampleForms(): List<SampleForm> {
+        return dbQuery.getAllSampleForms(::mapSampleForm).executeAsList()
+    }
+
+    //get the list of fields for specific form (to display for form field entry)
+    internal fun getFormFields(formId: Long): List<Field> {
+        return dbQuery.getFormFields(formId, ::mapField).executeAsList()
+    }
+
+    //get all sample data
+    internal fun getAllSampleData(): List<SampleData> {
+        return dbQuery.getAllSampleData(::mapSampleData).executeAsList()
+    }
+    //get all sample data from specific form
+    internal fun getSampleDataByForm(formId: Long): List<SampleData> {
+        return dbQuery.getSampleDataByForm(formId, ::mapSampleData).executeAsList()
+    }
+    //gets single sample by sample ID
+    internal fun getSampleData(sampleId: Long): SampleData {
+        return dbQuery.getSampleData(sampleId, ::mapSampleData).executeAsOne()
+    }
+
+    //get list of all data entries for a sample type
+    internal fun getDataEntry(sampleId: Long): List<DataEntry> {
+        return dbQuery.getDataEntry(sampleId, ::mapDataEntry).executeAsList()
+    }
+
+    //uses getSampleData and getDataEntry to create data class object SampleAndData (easier to use in frontend)
+    internal fun getSampleAndData(sampleId: Long): SampleAndData {
+        val sampleData = getSampleData(sampleId)
+        val dataEntries = getDataEntry(sampleId)
+        //converts list of data entries into map<Long, String> (iterates through list)
+        val dataEntriesMap = dataEntries.associate{it.fieldId.toLong() to it.userInput}
+
+        return SampleAndData(
+            sampleId = sampleData.sampleId,
+            formId = sampleData.formId,
+            dateCollectedUTC = sampleData.dateCollectedUTC,
+            location = sampleData.location,
+            dataEntries = dataEntriesMap
+        )
+    }
+}
+
+private fun mapSampleForm(
+    form_id: Long,
+    form_name: String
+): SampleForm {
+    return SampleForm(
+        formId = form_id.toInt(),
+        formName = form_name
+    )
+}
+
+// Maps db field data to kotlin class obj
+private fun mapField(
+    field_id: Long,
+    form_id: Long,
+    field_name: String,
+    order_num: Long,
+    field_type: String,
+    is_required: Long?,
+    options: String?,
+): Field {
+    return Field(
+        fieldId = field_id.toInt(),
+        formId = form_id.toInt(),
+        fieldName = field_name,
+        orderNum = order_num.toInt(),
+        fieldType = strToFT(field_type), // converts string to fieldType enum
+        isRequired = is_required == 1L,
+        options = jsonStringToList(options), // converts string to list<string>
+    )
+}
+
+// Maps db sample data to kotlin class obj
+private fun mapSampleData(
+    sample_id: Long,
+    form_id: Long,
+    date_collected_utc: String,
+    location: String
+): SampleData {
+    return SampleData(
+        sampleId = sample_id.toInt(),
+        formId = form_id.toInt(),
+        dateCollectedUTC = date_collected_utc,
+        location = location,
+    )
+}
+
+private fun mapDataEntry(
+    entry_id: Long,
+    sample_id: Long,
+    field_id: Long,
+    user_input: String
+): DataEntry {
+    return DataEntry(
+        entryId = entry_id.toInt(),
+        sampleId = sample_id.toInt(),
+        fieldId = field_id.toInt(),
+        userInput = user_input,
+    )
+}
+
+private fun strToFT(fieldType: String): FieldType {
+    return try {
+        FieldType.valueOf(fieldType.uppercase())
+    } catch (e: IllegalArgumentException) {
+        throw IllegalArgumentException("Unknown FieldType: $fieldType")
+    }
+}
+
+private fun listToJsonString(list: List<String>?): String? {
+    return list?.let { Json.encodeToString(it) }
+}
+
+private fun jsonStringToList(json: String?): List<String>? {
+    return json?.let { Json.decodeFromString(it) }
 }
---
+++ composeApp/src/commonMain/kotlin/com/dev/database/entity/Entity.kt
@@ -0,0 +1,58 @@
+package com.dev.database.entity
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+// type of fields that the user can add to their form
+@Serializable
+enum class FieldType {
+    SHORT_STRING,
+    LONG_STRING,
+    DROPDOWN,
+    MULTI_SELECT
+}
+
+// sampleForm
+@Serializable
+data class SampleForm(
+    @SerialName("form_id") val formId: Int,
+    @SerialName("form_name") val formName: String
+)
+
+@Serializable
+data class Field(
+    @SerialName("field_id") val fieldId: Int,
+    @SerialName("form_id") val formId: Int,
+    @SerialName("field_name") val fieldName: String,
+    @SerialName("order_num") val orderNum: Int,
+    @SerialName("field_type") val fieldType: FieldType,
+    @SerialName("is_required") val isRequired: Boolean,
+    @SerialName("options") val options: List<String>? = null, //list of options, if drop down or multi-select field type
+)
+
+// Stores actual form entries (Combines sample data and data entries for that sample)
+@Serializable
+data class SampleData(
+    @SerialName("sample_id") val sampleId: Int,
+    @SerialName("form_id") val formId: Int,
+    @SerialName("date_collected_utc") val dateCollectedUTC: String,
+    @SerialName("location") val location: String,
+)
+
+@Serializable
+data class DataEntry(
+    @SerialName("entry_id") val entryId: Int,
+    @SerialName("sample_id") val sampleId: Int, //Links to sample
+    @SerialName("field_id") val fieldId: Int, //Links to field that was filled out
+    @SerialName("user_input") val userInput: String, //Actual data, converted to string
+)
+
+// Stores actual form entries (Combines sample data and data entries for that sample)
+@Serializable
+data class SampleAndData(
+    @SerialName("sample_id") val sampleId: Int,
+    @SerialName("form_id") val formId: Int,
+    @SerialName("date_collected_utc") val dateCollectedUTC: String,
+    @SerialName("location") val location: String,
+    @SerialName("data_entries") val dataEntries: Map<Long, String> //First value contains field id, second value contains user input
+)
