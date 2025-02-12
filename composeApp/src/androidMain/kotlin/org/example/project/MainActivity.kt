4d171e914e5069737e9647c907470a35d0ff3162
Alexandra Seelig <108905816+lolxela@users.noreply.github.com>
2025-02-12 14:26:41 -0500

    Added new field to forms table to track if form is active or not (not active is equivalent to being deleted)

--- composeApp/src/androidMain/kotlin/org/example/project/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/example/project/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.lifecycle.lifecycleScope
 import com.dev.database.cache.AndroidDatabaseProvider
 import com.dev.database.cache.DatabaseProvider
-import com.dev.database.cache.DatabaseSetup
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
@@ -16,14 +15,6 @@ class MainActivity : ComponentActivity() {
         // Initialize database provider
         val databaseProvider = AndroidDatabaseProvider(applicationContext)
         DatabaseProvider.initialize(databaseProvider)
-
-        // Check if we need to create the default form
-        lifecycleScope.launch {
-            val database = databaseProvider.database
-            if (database.getAllSampleForms().isEmpty()) {
-                DatabaseSetup.createDefaultSampleForm(database)
-            }
-        }
 
         setContent {
             App()
